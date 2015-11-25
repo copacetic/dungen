d08801df2b7d328519d51475f690a5620f0fed90
Hayg Astourian <haygastourian@gmail.com>
2015-11-24 20:02:27 -0800

    Make it fail at modded prims... still looks cool...

--- dungen.py
+++ dungen.py
@@ -11,17 +11,25 @@ import sys
 import signal
 
 def signalHandler(signal, frame):
-  print "SIGINT received"
+  print("SIGINT received")
   sys.exit()
 
 SIZE = 50
 SCREEN_WIDTH, SCREEN_HEIGHT = 600.0, 600.0
 X_STEP_SIZE = SCREEN_WIDTH / SIZE
 Y_STEP_SIZE = SCREEN_HEIGHT / SIZE
+DIRECTIONS = [(1,0),(0,1),(-1,0),(0,-1)]
 
 class Tile:
-   def __init__(self):
-     self.occupied = bool(random.getrandbits(1))
+  def __init__(self, occupied=None):
+    if occupied != None:
+      self.occupied = occupied
+    else:
+      self.occupied = bool(random.getrandbits(1))
+
+  def setOccupancy(self, occupancy):
+    self.occupied = occupancy
+    return self
 
 def drawGrid(grid):
     #(0, SCREEN_HEIGHT)  (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -58,16 +66,56 @@ def drawGrid(grid):
     glDisableClientState(GL_VERTEX_ARRAY)
     glutSwapBuffers();
 
+def isInboundCoord(x, y):
+  return (x >= 0 and x < SIZE) and (y >= 0 and y < SIZE)
+
+def getWalls(grid, cell):
+  walls = []
+  x, y = cell[0], cell[1]
+  for direction in DIRECTIONS:
+    newX = x + direction[0]
+    newY = y + direction[1]
+    if isInboundCoord(newX, newY) and grid[newX][newY].occupied:
+      walls.append((newX, newY))
+  return walls
+
+def modded_prims():
+  if SIZE <= 0:
+    print("Error: SIZE is <= 0")
+
+  grid = [[Tile(occupied=True) for _x in range(SIZE)] for _y in range(SIZE)]
+  picked = grid[0][0]
+  maze = set([(0,0)])
+  walls = getWalls(grid, (0,0))
+  while len(walls) > 0:
+    randWall = random.sample(walls, 1)[0]
+    randDirection = random.sample(DIRECTIONS, 1)[0]
+    randNeighborX = randWall[0] + randDirection[0]
+    randNeighborY = randWall[1] + randDirection[1]
+
+    if isInboundCoord(randNeighborX, randNeighborY) and ((randNeighborX, randNeighborY) not in maze):
+      # add both the wall we picked and the wall in the rand direction to the maze
+      maze.add(randWall)
+      maze.add((randNeighborX, randNeighborY))
+
+      walls.extend(getWalls(grid, (randNeighborX, randNeighborY)))
+
+    # set cells in maze as unoccupied, for rendering later
+    for cell in maze:
+      grid[cell[0]][cell[1]].setOccupancy(False)
+    walls.remove(randWall)
+    yield grid
+
 def initSimulation():
-  grid = [[Tile() for _ in range(SIZE)] for _ in range(SIZE)]
-  return grid
+  return modded_prims()
 
 def display():
-  grid = initSimulation()
-  drawGrid(grid)
+  gen = initSimulation()
+  for grid in gen:
+    drawGrid(grid)
 
 def initGL():
-  print "Initializing opengl..."
+  print("Initializing opengl...")
   glClearColor(1.0,1.0,1.0,0.0)
   glColor3f(0.0,0.0, 0.0)
   glMatrixMode(GL_MODELVIEW)
@@ -75,11 +123,11 @@ def initGL():
   gluOrtho2D(0.0,int(SCREEN_WIDTH),0.0,(SCREEN_HEIGHT))
 
 def main():
-  print "Registering exit signal handler..."
+  print("Registering exit signal handler...")
   signal.signal(signal.SIGINT, signalHandler)
   glutInit()
   glutInitWindowSize(int(SCREEN_WIDTH),int(SCREEN_HEIGHT))
-  print "Creating window..."
+  print("Creating window...")
   glutCreateWindow("Copacetic's Dungeon Generator")
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
   glutDisplayFunc(display)
